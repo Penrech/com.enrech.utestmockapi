ef2b461586503a4f9225eda83513966bcf0bc5dc
Pau Enrech <pauenrech94@gmail.com>
2023-09-09 14:17:34 +0200

    Init commit

---
+++ src/main/kotlin/com/enrech/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package com.enrech.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
